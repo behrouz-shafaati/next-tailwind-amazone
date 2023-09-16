431d6c22503fbbdfba4d434867e3ce0239deef11
Behrouz Shafaati <behrouz.shafaati@gmail.com>
2023-09-16 21:17:24 +0330

    Create product item and show product list n home page from data.js file

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,19 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Layout from '@/components/Layout';
+import data from '@/utils/data';
+import ProductItem from '@/components/ProductItem';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <Layout title="Home Page">Home page</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-5">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug} />
+        ))}
+      </div>
+    </Layout>
+  );
 }

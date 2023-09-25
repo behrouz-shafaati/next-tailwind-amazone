577d8fab75f2f02964aac74e67a2e2da227dc0b8
Behrouz Shafaati <behrouz.shafaati@gmail.com>
2023-09-25 15:54:06 +0330

    Load product mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,50 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import Layout from '@/components/Layout';
-import data from '@/utils/data';
-import ProductItem from '@/components/ProductItem';
+import axios from 'axios';
+import { useContext } from 'react';
+import { toast } from 'react-toastify';
+import Layout from '../components/Layout';
+import ProductItem from '../components/ProductItem';
+import Product from '../models/Product';
+import db from '../utils/db';
+import { Store } from '../utils/Store';
 
-const inter = Inter({ subsets: ['latin'] });
+export default function Home({ products }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
 
-export default function Home() {
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      return toast.error('Sorry. Product is out of stock');
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+
+    toast.success('Product added to the cart');
+  };
+
   return (
     <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-5">
-        {data.products.map((product) => (
-          <ProductItem product={product} key={product.slug} />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {products.map((product) => (
+          <ProductItem
+            product={product}
+            key={product.slug}
+            addToCartHandler={addToCartHandler}
+          ></ProductItem>
         ))}
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [], shippingAddress: {} },
+    : { cartItems: [], shippingAddress: {}, PaymentMethod: '' },
 };
 
 function reducer(state, action) {

03fa8fd07b03dbcb1201d967f12417129c499cf1
Behrouz Shafaati <behrouz.shafaati@gmail.com>
2023-09-28 07:31:07 +0330

    Create order history screen

---
+++ pages/api/orders/history.js
@@ -0,0 +1,16 @@
+import { getToken } from 'next-auth/jwt';
+import Order from '../../../models/Order';
+import db from '../../../utils/db';
+
+const handler = async (req, res) => {
+  const user = await getToken({ req, secret: process.env.SECRET });
+  if (!user) {
+    return res.status(401).send('signin required');
+  }
+  await db.connect();
+  const orders = await Order.find({ user: user._id });
+  await db.disconnect();
+  res.send(orders);
+};
+
+export default handler;
